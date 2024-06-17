81e60aaae5929170d14cfabe083da4d0188e1aaa
MABIRUS <killing.ak56@gmail.com>
2024-06-17 13:15:23 +0300

    refactor code

--- MainSceletonBot.Tests/DataSystemTests/ModelTests/IDSetterTests.cs
+++ MainSceletonBot.Tests/DataSystemTests/ModelTests/IDSetterTests.cs
@@ -7,70 +7,70 @@ using MainSceletonBot.Scripts.DataSystem.Model;
 
 namespace MainSceletonBot.Tests.DataSystemTests.ModelTests
 {
-    public class IDSetterTests
+  public class IDSetterTests
+  {
+    [Fact]
+    public void Given_new_customer_When_get_id_Then_unique_id_is_returned()
     {
-        [Fact]
-        public void Given_new_customer_When_get_id_Then_unique_id_is_returned()
-        {
-            // Arrange
-            IDSetter.RestoreIDSetter();
+      // Arrange
+      IDSetter.RestoreIDSetter();
 
-            // Act
-            var id1 = IDSetter.GetCustomerID();
-            var id2 = IDSetter.GetCustomerID();
+      // Act
+      var id1 = IDSetter.GetCustomerID();
+      var id2 = IDSetter.GetCustomerID();
 
-            // Assert
-            Assert.NotEqual(id1, id2);
-            Assert.Equal("1", id1);
-            Assert.Equal("2", id2);
-        }
-
-        [Fact]
-        public void Given_new_customer_task_When_get_id_Then_unique_id_is_returned()
-        {
-            // Arrange
-            IDSetter.RestoreIDSetter();
-
-            // Act
-            var id1 = IDSetter.GetCustomerTaskID();
-            var id2 = IDSetter.GetCustomerTaskID();
-
-            // Assert
-            Assert.NotEqual(id1, id2);
-            Assert.Equal("1", id1);
-            Assert.Equal("2", id2);
-        }
-
-        [Fact]
-        public void Given_new_worker_When_get_id_Then_unique_id_is_returned()
-        {
-            // Arrange
-            IDSetter.RestoreIDSetter();
-
-            // Act
-            var id1 = IDSetter.GetWorkerID();
-            var id2 = IDSetter.GetWorkerID();
-
-            // Assert
-            Assert.NotEqual(id1, id2);
-            Assert.Equal("1", id1);
-            Assert.Equal("2", id2);
-        }
-
-        [Fact]
-        public void Given_id_setter_When_restore_Then_counters_are_reset()
-        {
-            // Act
-            IDSetter.RestoreIDSetter();
-
-            var customerID = IDSetter.GetCustomerID();
-            var customerTaskID = IDSetter.GetCustomerTaskID();
-            var workerID = IDSetter.GetWorkerID();
-
-            // Assert
-            Assert.Equal("1", customerID);
-            Assert.Equal("1", customerTaskID);
-            Assert.Equal("1", workerID);
-        }
+      // Assert
+      Assert.NotEqual(id1, id2);
+      Assert.Equal("1", id1);
+      Assert.Equal("2", id2);
     }
+
+    [Fact]
+    public void Given_new_customer_task_When_get_id_Then_unique_id_is_returned()
+    {
+      // Arrange
+      IDSetter.RestoreIDSetter();
+
+      // Act
+      var id1 = IDSetter.GetCustomerTaskID();
+      var id2 = IDSetter.GetCustomerTaskID();
+
+      // Assert
+      Assert.NotEqual(id1, id2);
+      Assert.Equal("1", id1);
+      Assert.Equal("2", id2);
+    }
+
+    [Fact]
+    public void Given_new_worker_When_get_id_Then_unique_id_is_returned()
+    {
+      // Arrange
+      IDSetter.RestoreIDSetter();
+
+      // Act
+      var id1 = IDSetter.GetWorkerID();
+      var id2 = IDSetter.GetWorkerID();
+
+      // Assert
+      Assert.NotEqual(id1, id2);
+      Assert.Equal("1", id1);
+      Assert.Equal("2", id2);
+    }
+
+    [Fact]
+    public void Given_id_setter_When_restore_Then_counters_are_reset()
+    {
+      // Act
+      IDSetter.RestoreIDSetter();
+
+      var customerID = IDSetter.GetCustomerID();
+      var customerTaskID = IDSetter.GetCustomerTaskID();
+      var workerID = IDSetter.GetWorkerID();
+
+      // Assert
+      Assert.Equal("1", customerID);
+      Assert.Equal("1", customerTaskID);
+      Assert.Equal("1", workerID);
+    }
+  }
 }
--- MainSceletonBot.Tests/DataSystemTests/ModelTests/WorkerTests.cs
+++ MainSceletonBot.Tests/DataSystemTests/ModelTests/WorkerTests.cs
@@ -7,62 +7,62 @@ using MainSceletonBot.Scripts.DataSystem.Model;
 
 namespace MainSceletonBot.Tests.DataSystemTests.ModelTests
 {
-    public class WorkerTests
+  public class WorkerTests
+  {
+    [Fact]
+    public void Given_few_new_workers_When_initialized_Then_each_has_unique_ID()
     {
-        [Fact]
-        public void Given_few_new_workers_When_initialized_Then_each_has_unique_ID()
-        {
-            // Act
-            var worker1 = new Worker();
-            var worker2 = new Worker();
+        // Act
+        var worker1 = new Worker();
+        var worker2 = new Worker();
 
-            // Assert
-            Assert.NotEqual(worker1.ID, worker2.ID);
-        }
-
-        [Fact]
-        public void Given_customer_task_When_set_task_in_work_Then_IsBussy_is_true()
-        {
-            // Arrange
-            var worker = new Worker();
-            var task = new CustomerTask("Test Task");
-
-            // Act
-            worker.SetTaskInWork(task);
-
-            // Assert
-            Assert.True(worker.IsBussy);
-            Assert.Equal(task, worker.TaskInWork);
-        }
-
-        [Fact]
-        public void Given_worker_with_task_When_task_finished_Then_IsBussy_is_false()
-        {
-            // Arrange
-            var worker = new Worker();
-            var task = new CustomerTask("Test Task");
-            worker.SetTaskInWork(task);
-
-            // Act
-            worker.FinishTaskInWork();
-
-            // Assert
-            Assert.False(worker.IsBussy);
-        }
-
-        [Fact]
-        public void Given_worker_with_task_When_task_finished_Then_TaskInWork_is_null()
-        {
-            // Arrange
-            var worker = new Worker();
-            var task = new CustomerTask("Test Task");
-            worker.SetTaskInWork(task);
-
-            // Act
-            worker.FinishTaskInWork();
-
-            // Assert
-            Assert.Null(worker.TaskInWork);
-        }
+        // Assert
+        Assert.NotEqual(worker1.ID, worker2.ID);
     }
+
+    [Fact]
+    public void Given_customer_task_When_set_task_in_work_Then_IsBussy_is_true()
+    {
+        // Arrange
+        var worker = new Worker();
+        var task = new CustomerTask("Test Task");
+
+        // Act
+        worker.SetTaskInWork(task);
+
+        // Assert
+        Assert.True(worker.IsBussy);
+        Assert.Equal(task, worker.TaskInWork);
+    }
+
+    [Fact]
+    public void Given_worker_with_task_When_task_finished_Then_IsBussy_is_false()
+    {
+        // Arrange
+        var worker = new Worker();
+        var task = new CustomerTask("Test Task");
+        worker.SetTaskInWork(task);
+
+        // Act
+        worker.FinishTaskInWork();
+
+        // Assert
+        Assert.False(worker.IsBussy);
+    }
+
+    [Fact]
+    public void Given_worker_with_task_When_task_finished_Then_TaskInWork_is_null()
+    {
+        // Arrange
+        var worker = new Worker();
+        var task = new CustomerTask("Test Task");
+        worker.SetTaskInWork(task);
+
+        // Act
+        worker.FinishTaskInWork();
+
+        // Assert
+        Assert.Null(worker.TaskInWork);
+    }
+  }
 }
