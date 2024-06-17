164b8152a3d99a8317fc8678291db27da894c811
Mikhail Safronov <misha.safronov.02.12@mail.ru>
2024-06-17 14:08:28 +0300

    usings drop

--- MainSceletonBot/Scripts/CommandSystem/CommandAndContextHolder.cs
+++ MainSceletonBot/Scripts/CommandSystem/CommandAndContextHolder.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MainSceletonBot.Scripts.CommandSystem
+﻿namespace MainSceletonBot.Scripts.CommandSystem
 {
   public abstract class CommandAndContextHolder<T>
   {
--- MainSceletonBot/Scripts/DataSystem/Model/CustomerTaskQueue.cs
+++ MainSceletonBot/Scripts/DataSystem/Model/CustomerTaskQueue.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MainSceletonBot.Scripts.DataSystem.Model
+﻿namespace MainSceletonBot.Scripts.DataSystem.Model
 {
   public class CustomerTaskQueue
   {
